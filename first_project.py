53580554514564947709ce22a05a9e9a9fda7768
korbengardner-gif <korben.gardner@ucas-edu.net>
2026-08-18 09:43:24 -0600

    larose

---
+++ first_project.py
@@ -0,0 +1,11 @@
+#Korben Gardner, First Project
+
+name = input("What is your name: ")
+
+
+print("Welcome to class " + name)
+
+print("What did you do this weekend: ")
+
+weekend = input()
+print(+weekend+ "you say? well, " +name+ ", this weekend I just sat in a classroom collecting dust.")
