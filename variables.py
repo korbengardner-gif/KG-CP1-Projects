552216d804664f3479397eeaa660a20862be40ec
korbengardner-gif <korben.gardner@ucas-edu.net>
2026-08-20 09:05:08 -0600

    variablesExamples

---
+++ variables.py
@@ -0,0 +1,20 @@
+#Korben Gardner, variables notes
+
+# Create a variables
+name = "Katie"
+
+print("Hello " +name)
+
+#reset variable
+name = "Alex"
+
+print("Sorry I meant " +name)
+
+
+# another variable example
+age = 14
+
+print("Wow you are", age)
+print("Oh wait your birthday was yesterday?")
+age += 1
+print("So you are", age)
