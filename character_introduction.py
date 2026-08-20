668faa1fcf6ec81206647a6794bca8549609b8bf
korbengardner-gif <korben.gardner@ucas-edu.net>
2026-08-20 09:38:35 -0600

    Made character intro

---
+++ character_introduction.py
@@ -0,0 +1,8 @@
+#Korben Gardner, character introduction project
+name = "Joe"
+age = "30"
+job = "engineer"
+home = "Texas"
+
+
+print("Hi, my name is " +name+ ", I am " +age+ ", I work as a " +job+ ", and I'm from " +home)
