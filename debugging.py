4035b917ea16b778d867ba812843dd42c3f6a5b3
korbengardner-gif <korben.gardner@ucas-edu.net>
2026-08-24 09:12:52 -0600

    debugging assignment

---
+++ debugging.py
@@ -0,0 +1,6 @@
+#Korben Gardner, debugging code
+
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am ",age," years old.") #close paranthesis, E at the end of "name", switched "str()" with comas
