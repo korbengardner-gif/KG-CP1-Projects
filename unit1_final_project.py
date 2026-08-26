0d7207794eea646c6e9155769af65e9542fc3754
korbengardner-gif <korben.gardner@ucas-edu.net>
2026-08-26 09:52:35 -0600

    final project

---
+++ unit1_final_project.py
@@ -0,0 +1,11 @@
+#Korben Gardner, unit 1 final project
+
+
+name = input("What's your name? ")
+candy = input("What's your favorite candy? ")
+event = input("What's your favorite sport or game? ")
+hangout = input("Where do you like to go with your best friend? ")
+car = input("What's your favorite car? ")
+friend = input("Who is your best friend? ")
+
+print(name + " likes " + candy + ", " + event + ", " + car + ", and to be with " + friend +" at " +hangout)
