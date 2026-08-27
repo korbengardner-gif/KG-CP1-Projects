5c6089221304a1373d9bf0e72ce711476a5d7f17
korbengardner-gif <korben.gardner@ucas-edu.net>
2026-08-27 09:07:39 -0600

    Finished final project

--- unit1_final_project.py
+++ unit1_final_project.py
@@ -4,8 +4,8 @@
 name = input("What's your name? ")
 candy = input("What's your favorite candy? ")
 event = input("What's your favorite sport or game? ")
-hangout = input("Where do you like to go with your best friend? ")
 car = input("What's your favorite car? ")
 friend = input("Who is your best friend? ")
+hangout = input("Where do you like to go with your best friend? ")
 
 print(name + " likes " + candy + ", " + event + ", " + car + ", and to be with " + friend +" at " +hangout)
