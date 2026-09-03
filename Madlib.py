18fd276a135b0f0bafd8594726db6bd156af7a9f
korbengardner-gif <korben,gardner@ucas-edu.net>
2026-09-03 09:43:27 -0600

    fixed average grade

--- Madlib.py
+++ Madlib.py
@@ -1,8 +1,14 @@
 #Korben Gardner, Madlib assignment
 
 
-adjective1 = input("Give me an Adjective ")
-noun1 = input("Give me a Noun ")
-noun2 = input("Give another Noun ")
-verb1 = input("Give me a Verb that DOESN'T end in ING ")
-adjective2 = input("Give me another Adjective ")
+adjective1 = input("Give me an Adjective: ")
+noun1 = input("Give me a Noun: ")
+noun2 = input("Give another Noun: ")
+verb1 = input("Give me a Verb that DOESN'T end in ING: ")
+adjective2 = input("Give me another Adjective: ")
+color = input("Give me a Color: ")
+exclamation = input("Give an exclamation/something yu would shout: ")
+noun3 = input("Give another Noun: ")
+verb2 = input("Give me a Verb that DOES end in ING: ")
+
+message = "Princess Zelda sent Link on a(n) " + adjective1 + " quest across the kingdom of Hyrule. To prepare, Link packed his trusty Master Sword, a shield, and a mysterious "
--- average_grade.py
+++ average_grade.py
@@ -1,40 +1,53 @@
 #Korben Gardner, average grade assignment
-
-try:
-    class1 = float(input("What is your grade in 1st period: "))
-except: 
-    ("That's not a number!")
-else:
-    
+while True:
+    try:
+        class1 = float(input("What is your grade in 1st period: "))
+    except: 
+        print("That's not a number!")
+    else:
+        break
+while True:  
     try:
         class2 = float(input("What is your grade in 2nd period: "))
     except:
-        ("That's not a number!")
+        print("That's not a number!")
     else:
-        try:
-            class3 = float(input("What is your grade in 3rd period: "))
-        except:
-            ("That's not a number!")
-        else:
-            try:
-                class45 = float(input("What is your grade in 4th/5th period: "))
-            except:
-                ("That's not a number!")
-            else:
-                try:
-                    class6 = float(input("What is your grade in 6th period: "))
-                except:
-                        ("That's not a number")
-                else:
-                    try:
-                        class7 = float(input("What is your grade in 7th period: "))
-                    except:
-                        ("That's not a number!")
-                    else:
-                        try:
-                            class8 = float(input("What is your grade in 8th period: "))
-                        except:
-                            ("That's not a number!")
+        break
+while True:
+    try:
+        class3 = float(input("What is your grade in 3rd period: "))
+    except:
+        print("That's not a number!")
+    else:
+        break
+while True:
+    try:
+        class45 = float(input("What is your grade in 4th/5th period: "))
+    except:
+        print("That's not a number!")
+    else:
+        break
+while True:
+    try:
+        class6 = float(input("What is your grade in 6th period: "))
+    except:
+        print("That's not a number")
+    else:
+        break
+while True:
+    try:
+        class7 = float(input("What is your grade in 7th period: "))
+    except:
+        print("That's not a number!")
+    else:
+        break
+while True:
+    try:
+        class8 = float(input("What is your grade in 8th period: "))
+    except:
+        print("That's not a number!")
+    else:
+        break
                     
 
 
