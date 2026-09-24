473fc6bef7a51712be54b5d0d4fa6ba40f1fe828
korbengardner-gif <korben.gardner@ucas-edu.net>
2026-09-24 16:21:22 -0600

    fixed code

--- crew_share.py
+++ crew_share.py
@@ -1,7 +1,7 @@
 #KG crew shares assignment
 import random
 
-start_units = random.randint(50000,500000)
+start_units = random.randint(500,5000)
 
 while True:
     try:
@@ -17,17 +17,17 @@ yondu_cut = round(units * 0.13, 2)
 
 after_yonducut = units - yondu_cut
 
-peter_cut = round(after_yonducut * 0.11)
+peter_cut = round(after_yonducut * 0.11, 2)
 
 after_petercut = after_yonducut - peter_cut
 
-crew_share = round(after_petercut / (crew + 2))
+crew_share = round(after_petercut / (crew + 2), 2)
 
 yondu_total =  yondu_cut + crew_share
 peter_total =  peter_cut + crew_share
 
 print(f"There are {crew + 2} pirates.")
-print(f"They have {units} units.")
+print(f"They have {start_units} units.")
 print(f"Yondu gets {yondu_total} units!")
 print(f"Peter gets {peter_total} units!")
-print(f"The rest of the crew gets {crew_share + 3} units!")
+print(f"The rest of the crew gets {crew_share} units!")
